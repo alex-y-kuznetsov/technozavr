0bd922909e4e9b4eea0879e97b47e3f127d3516c
Alex <alex.y.kuznetsov@gmail.com>
2020-07-15 15:38:58 +0500

    product description

---
+++ src/components/ProductDescription.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="item__desc">
+    <ul class="tabs">
+      <li v-for="description in product.descriptions"
+          v-bind:key="description.type" class="tabs__item">
+        <a class="tabs__link" href="#"
+           v-bind:class="{'tabs__link--current': shownDescription === description.type}"
+           v-on:click.prevent="showDescription(description.type)">
+          {{ description.title }}
+        </a>
+      </li>
+    </ul>
+    <div v-for="description in product.descriptions"
+         v-bind:key="description.type"
+         v-show="shownDescription === description.type" class="item__content">
+      <p>
+        {{ description.text }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import products from '@/data/products';
+
+export default {
+  props: {
+    pageParams: {
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      product: {},
+      shownDescription: 'general',
+    };
+  },
+  methods: {
+    getProductDescriptions() {
+      return products.find((product) => product.id === this.pageParams.id);
+    },
+    showDescription(type) {
+      this.shownDescription = type;
+    },
+  },
+  created() {
+    this.product = this.getProductDescriptions();
+  },
+};
+</script>
--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -95,76 +95,7 @@
           </form>
         </div>
       </div>
-
-      <div class="item__desc">
-        <ul class="tabs">
-          <li class="tabs__item">
-            <a class="tabs__link tabs__link--current">
-              Описание
-            </a>
-          </li>
-          <li class="tabs__item">
-            <a class="tabs__link" href="#">
-              Характеристики
-            </a>
-          </li>
-          <li class="tabs__item">
-            <a class="tabs__link" href="#">
-              Гарантия
-            </a>
-          </li>
-          <li class="tabs__item">
-            <a class="tabs__link" href="#">
-              Оплата и доставка
-            </a>
-          </li>
-        </ul>
-
-        <div class="item__content">
-          <p>
-            Навигация GPS, ГЛОНАСС, BEIDOU Galileo и QZSS<br>
-            Синхронизация со смартфоном<br>
-            Связь по Bluetooth Smart, ANT+ и Wi-Fi<br>
-            Поддержка сторонних приложений<br>
-          </p>
-
-          <a href="#">
-            Все характеристики
-          </a>
-
-          <h3>Что это?</h3>
-
-          <p>
-            Wahoo ELEMNT BOLT GPS – это велокомпьютер,
-            который позволяет оптимизировать свои велотренировки,
-            сделав их максимально эффективными. Wahoo ELEMNT BOLT GPS
-            синхронизируется с датчиками по ANT+, объединяя полученную с
-            них информацию. Данные отображаются на дисплее, а также сохраняются
-            на смартфоне. При этом на мобильное устройство можно установить
-            как фирменное приложение, так и различные приложения сторонних разработчиков.
-            Велокомпьютер точно отслеживает местоположение, принимая
-            сигнал с целого комплекса спутников.
-            Эта информация позволяет смотреть уже преодоленные маршруты
-            и планировать новые велопрогулки.
-          </p>
-
-          <h3>Дизайн</h3>
-
-          <p>
-            Велокомпьютер Wahoo ELEMNT BOLT очень компактный.
-            Размеры устройства составляют всего 74,6 x 47,3 x 22,1 мм.
-            что не превышает габариты смартфона. Корпус гаджета выполнен из
-            черного пластика. На обращенной к пользователю стороне расположен
-            дисплей диагональю 56 мм. На дисплей выводятся координаты и скорость,
-            а также полученная со смартфона и синхронизированных датчиков информация:
-            интенсивность, скорость вращения педалей, пульс и т.д.
-            (датчики не входят в комплект поставки).
-            Корпус велокомпьютера имеет степень защиты от влаги IPX7. Это означает,
-            что устройство не боится пыли, а также выдерживает кратковременное (до 30 минут)
-            погружение в воду на глубину не более 1 метра.
-          </p>
-        </div>
-      </div>
+      <ProductDescription v-bind:page-params="pageParams"/>
     </section>
   </main>
 </template>
@@ -172,10 +103,12 @@
 <script>
 import products from '@/data/products';
 import categories from '@/data/categories';
+import ProductDescription from '@/components/ProductDescription.vue';
 import goToPage from '@/helpers/goToPage';
 import numberFormat from '@/helpers/numberFormat';
 
 export default {
+  components: { ProductDescription },
   props: {
     pageParams: {
       type: Object,
