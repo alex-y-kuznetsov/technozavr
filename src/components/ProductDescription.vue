3fd6552ef53f4808138b15f283af6b2da582cbe8
Alex <alex.y.kuznetsov@gmail.com>
2020-07-18 18:11:20 +0500

    dynamic product description templates

--- src/components/ProductDescription.vue
+++ src/components/ProductDescription.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="item__desc">
     <ul class="tabs">
-      <li v-for="description in product.descriptions"
-          v-bind:key="description.type" class="tabs__item">
-        <a class="tabs__link" href="#"
-           v-bind:class="{'tabs__link--current': shownDescription === description.type}"
-           v-on:click.prevent="showDescription(description.type)">
-          {{ description.title }}
-        </a>
+      <li
+        v-for="description in product.descriptions"
+        v-bind:key="description.type"
+        class="tabs__item"
+      >
+        <a
+          class="tabs__link"
+          href="#"
+          v-bind:class="{'tabs__link--current': shownDescription === description.type}"
+          v-on:click.prevent="showDescription(description.type)"
+        >{{ description.title }}</a>
       </li>
     </ul>
-    <div v-for="description in product.descriptions"
+    <!--     <div v-for="description in product.descriptions"
          v-bind:key="description.type"
          v-show="shownDescription === description.type" class="item__content">
       <div v-html="description.text"></div>
-    </div>
+    </div>-->
+    <component v-bind:is="currentShownDescription.component" />
   </div>
 </template>
 
@@ -30,6 +35,7 @@ export default {
   data() {
     return {
       product: {},
+      tabs: [],
       shownDescription: 'general',
     };
   },
@@ -40,9 +46,23 @@ export default {
     showDescription(type) {
       this.shownDescription = type;
     },
+    getDescriptionTabs() {
+      this.tabs = this.product.descriptions.map((description) => ({
+        name: description.type,
+        component: {
+          template: `<div>${description.text}</div>`,
+        },
+      }));
+    },
+  },
+  computed: {
+    currentShownDescription() {
+      return this.tabs.find((tab) => tab.name === this.shownDescription);
+    },
   },
   created() {
     this.product = this.getProductDescriptions();
+    this.getDescriptionTabs();
   },
 };
 </script>
