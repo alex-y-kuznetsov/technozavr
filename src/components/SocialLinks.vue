c38039b69a0bbd6cb0042e92dd569441ab661eb5
Alex <alex.y.kuznetsov@gmail.com>
2020-08-19 22:56:42 +0500

    social hrefs

--- src/components/SocialLinks.vue
+++ src/components/SocialLinks.vue
@@ -1,7 +1,7 @@
 <template>
 <ul class="footer__social social">
   <li v-for="socialLink in social" v-bind:key="socialLink.name" class="social__item">
-    <a class="social__link" href="#" v-bind:aria-label="socialLink.name">
+    <a class="social__link" v-bind:href="socialLink.href" v-bind:aria-label="socialLink.name">
       <svg v-bind:width="socialLink.width" v-bind:height="socialLink.height" fill="currentColor">
         <use v-bind:xlink:href="socialLink.xlink" />
       </svg>
